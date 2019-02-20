fd2555e4efb7fe643752ece2e08ec0cc2654baf7
Nolan Braman <nolan.braman@gmail.com>
2019-02-20 18:14:01 -0500

    deleted cache

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
+
 import SEO from "../components/seo";
 import { Button, Grid, Row, Column, Icon } from "semantic-ui-react";
 
