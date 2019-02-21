d1d93b10ddb309ab24bdc82ee48c3e079aa5c7ad
Nolan Braman <nolan.braman@gmail.com>
2019-02-21 08:24:59 -0500

    banner issues

--- src/components/images/BannerImage.js
+++ src/components/images/BannerImage.js
@@ -17,7 +17,7 @@ const BannerImage = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "banner2.png" }) {
+        placeholderImage: file(relativePath: { eq: "banner2.webp" }) {
           childImageSharp {
             fluid(maxWidth: 3000) {
               ...GatsbyImageSharpFluid
--- src/components/images/BannerImageSmall.js
+++ src/components/images/BannerImageSmall.js
@@ -17,7 +17,7 @@ const BannerImage = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "banner3.png" }) {
+        placeholderImage: file(relativePath: { eq: "banner3.webp" }) {
           childImageSharp {
             fluid(maxWidth: 3000) {
               ...GatsbyImageSharpFluid
