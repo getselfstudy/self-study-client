f792e22311646f7736a036bdccac8774d6dcdbcc
Nolan Braman <nolan.braman@gmail.com>
2019-02-22 16:36:22 -0500

    cpca final test

--- src/components/Cpca.js
+++ src/components/Cpca.js
@@ -12,16 +12,20 @@ export class Cpca extends Component {
           <Grid columns={3} relaxed stackable>
             <Grid.Row>
               <Grid.Column width={5}>
-                <div className="cpca-sub container-positioning-outer">
+                <div className="cpca-sub ">
                   <div className="container-positioning-inner">
                     <CpcaPractice />
                   </div>
                 </div>
               </Grid.Column>
               <Grid.Column width={7}>
-                <h3>CPCA Practice Assessment I$30</h3>
+                <h3 style={{ paddingTop: "20px" }}>
+                  CPCA Practice Assessment I $30
+                </h3>
                 <h3>CPCA Practice Assessment II $30</h3>
-                <h3>CPCA Practice Assessment I & II $50 </h3>
+                <h3 style={{ paddingBottom: "20px" }}>
+                  CPCA Practice Assessment I & II $50{" "}
+                </h3>
               </Grid.Column>
               <Grid.Column width={4}>
                 <a href="https://www.nbcrna.com/exams/cpce/practice-exam">
@@ -38,6 +42,11 @@ export class Cpca extends Component {
             </Grid.Row>
           </Grid>
         </div>
+        <p style={{ fontSize: ".7em" }}>
+          {" "}
+          Not part of SelfStudyPLUS membership. If you are purchasing CPCA
+          Practice a second time, you can use your previous login.
+        </p>
       </div>
     );
   }
